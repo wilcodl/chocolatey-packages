684f3565e7f3527f3ba8239d59f2dce19b00067a
Wilco de Lange <wilcodl@gmail.com>
2021-01-11 21:52:38 +0100

    first commit

---
+++ eset-nod32/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+$url        = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
+$url64      = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  url64bit       = $url64
+  softwareName   = 'ESET Security'
+  silentArgs     = '--silent --accepteula --msi-property-ehs PRODUCTTYPE=eav'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
