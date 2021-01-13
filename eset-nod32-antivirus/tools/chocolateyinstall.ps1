7053814dfe0cd46a2e6257cd58ddf7121ac6b0a4
Wilco de Lange <wilcodl@gmail.com>
2021-01-13 16:51:09 +0100

    small changes

--- eset-nod32-antivirus/tools/chocolateyinstall.ps1
+++ eset-nod32-antivirus/tools/chocolateyinstall.ps1
@@ -1,28 +1,25 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$toolsDir   = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$toolsDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 [double]$WinVersion = [Environment]::OSVersion.Version.Major.toString() + "." + [Environment]::OSVersion.Version.Minor.toString()
 
 if ($WinVersion -lt 6.1){
-	Write-Error 'Windows version not supported'
+	throw 'Windows version not supported'
 }
 
 $OS = Get-CimInstance -ClassName Win32_OperatingSystem
 
 if ($OS.ProductType -ne 1){
-	Write-Error 'Only Windows client versions supported'
+	throw 'Only Windows client versions supported'
 }
-
-$url        = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
-$url64      = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
 
 $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
 	unzipLocation  = $toolsDir
 	fileType       = 'EXE'
-	url            = $url
-	url64bit       = $url64
+	url            = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
+	url64bit       = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
 	softwareName   = 'ESET Security'
 	silentArgs     = '--silent --accepteula --msi-property-ehs PRODUCTTYPE=eav'
 	validExitCodes = @(0)
