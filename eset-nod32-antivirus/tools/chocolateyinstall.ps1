40d535c6c63548200dfe2549abf2adb695f5770b
Wilco de Lange <wilcodl@gmail.com>
2021-01-17 11:17:46 +0100

    checksums added

--- eset-nod32-antivirus/tools/chocolateyinstall.ps1
+++ eset-nod32-antivirus/tools/chocolateyinstall.ps1
@@ -21,6 +21,10 @@ $packageArgs = @{
 	url            = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
 	url64bit       = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
 	softwareName   = 'ESET Security'
+	checksum       = '4C6919D7BB3BE0676F79A66B1F51597BA0CA53224D181CC90E85CC2BF7D0CA1A'
+	checksumType   = 'sha256'
+	checksum64     = 'CC568008E7DBFB9F8B93E35A167EEDCCDF72799C74A713961066EB0032D455C9'
+	checksumType64 = 'sha256'
 	silentArgs     = '--silent --accepteula --msi-property-ehs PRODUCTTYPE=eav'
 	validExitCodes = @(0)
 }
