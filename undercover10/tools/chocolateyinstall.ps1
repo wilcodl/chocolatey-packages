b992693af5e63e5d8f0bbf31bd1fcf1c85c42448
Wilco de Lange <wilcodl@gmail.com>
2021-02-09 21:43:41 +0100

    first version undercover10 added

---
+++ undercover10/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$fileLocation = Join-Path -Path $toolsDir -ChildPath 'setup.exe'
+
+$packageArgs = @{
+	packageName    = $env:ChocolateyPackageName
+	fileType       = 'exe'
+	file            = $fileLocation
+	softwareName   = 'UnderCover10 version 3.00'
+	silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+	validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
