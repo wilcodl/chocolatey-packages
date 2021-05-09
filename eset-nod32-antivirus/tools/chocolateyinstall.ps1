a05bbe1f4e4a179d0ec79dbc82dd96c354207303
Wilco de Lange <wilcodl@gmail.com>
2021-05-09 21:44:32 +0200

    eset-nod32-antivirus updated to 14.1.20.0

--- eset-nod32-antivirus/tools/chocolateyinstall.ps1
+++ eset-nod32-antivirus/tools/chocolateyinstall.ps1
@@ -21,9 +21,9 @@ $packageArgs = @{
 	url            = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
 	url64bit       = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
 	softwareName   = 'ESET Security'
-	checksum       = '4C6919D7BB3BE0676F79A66B1F51597BA0CA53224D181CC90E85CC2BF7D0CA1A'
+	checksum       = 'CF72BD8AB7B7B7EA53636D670BCAF322858C19EB268296607D478B40A6283E87'
 	checksumType   = 'sha256'
-	checksum64     = 'CC568008E7DBFB9F8B93E35A167EEDCCDF72799C74A713961066EB0032D455C9'
+	checksum64     = '46BFFE6D1A3128AD2C2EA0E6019AA41040F54BA03DDD7D54EED1E9A4826F3EBB'
 	checksumType64 = 'sha256'
 	silentArgs     = '--silent --accepteula --msi-property-ehs PRODUCTTYPE=eav'
 	validExitCodes = @(0)
