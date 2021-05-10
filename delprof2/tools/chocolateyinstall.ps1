1fee60f6e2fae08dca3f5711aaad35ad3c777754
Wilco de Lange <wilcodl@gmail.com>
2021-05-10 22:04:09 +0200

    delprof2 added

---
+++ delprof2/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+	packageName   = $env:ChocolateyPackageName
+	unzipLocation = $toolsDir
+	url           = 'https://helgeklein.com/downloads/DelProf2/current/Delprof2 1.6.0.zip'
+	checksum      = '14EE8BAF68AC5B015613ECDF23D1CC49A7045F791E2F7A1952F32AAD5B4A606B'
+	checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
