63a69625e5dcf96f4595fb9aa137354806c9125c
Wilco de Lange <wilcodl@gmail.com>
2021-09-27 22:08:07 +0200

    exitcode 5022 added and updated to 14.2.24

--- eset-nod32-antivirus/tools/chocolateyinstall.ps1
+++ eset-nod32-antivirus/tools/chocolateyinstall.ps1
@@ -14,6 +14,8 @@ if ($OS.ProductType -ne 1){
 	throw 'Only Windows client versions supported'
 }
 
+# checksum -t sha256 -f D:\eav_nt64_enu.exe
+
 $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
 	unzipLocation  = $toolsDir
@@ -21,12 +23,12 @@ $packageArgs = @{
 	url            = 'https://download.eset.com/download/win/eav/eav_nt32_enu.exe'
 	url64bit       = 'https://download.eset.com/download/win/eav/eav_nt64_enu.exe'
 	softwareName   = 'ESET Security'
-	checksum       = 'CF72BD8AB7B7B7EA53636D670BCAF322858C19EB268296607D478B40A6283E87'
+	checksum       = 'CCFE369A891122DD7C67D9BF322F7364E5FC7C7664CEA9E9431C0C2255FC6AAE'
 	checksumType   = 'sha256'
-	checksum64     = '46BFFE6D1A3128AD2C2EA0E6019AA41040F54BA03DDD7D54EED1E9A4826F3EBB'
+	checksum64     = '2CDE0B59852E5EE8C3279B08C14DBFD5918353ED03338A00C353A24C45275A54'
 	checksumType64 = 'sha256'
 	silentArgs     = '--silent --accepteula --msi-property-ehs PRODUCTTYPE=eav'
-	validExitCodes = @(0)
+	validExitCodes = @(0, 5022) # 5022: version already current
 }
 
 Install-ChocolateyPackage @packageArgs
