0474db5e61548fdb9c02e909e85a22dff28ef887
Wilco de Lange <wilcodl@gmail.com>
2022-10-28 17:41:02 +0200

    clickpaste added

---
+++ clickpaste/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+	packageName   = $env:ChocolateyPackageName
+	unzipLocation = $toolsDir
+	url           = 'https://github.com/Collective-Software/ClickPaste/releases/download/v1.0.1/ClickPaste_v1.0.1.zip'
+	checksum      = 'C6946857FC733CFF1417DEA2EEB46D3CCD03CFDE462DA3D3830ACB40B3335568'
+	checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
