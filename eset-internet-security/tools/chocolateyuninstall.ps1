2541123c722995d6f94ab79b27dea9dfe59f3457
Wilco de Lange <wilcodl@gmail.com>
2022-11-08 23:24:40 +0100

    clean

--- eset-internet-security/tools/chocolateyuninstall.ps1
+++ eset-internet-security/tools/chocolateyuninstall.ps1
@@ -1,3 +1,1 @@
-﻿# Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1" -Force
-
-Uninstall-ChocolateyPackage -packageName $env:ChocolateyPackageName -fileType MSI -silentArgs "{DC8185CE-C7E6-4812-9674-42919F445464} /qb /norestart" -validExitCodes @(0, 3010)
+﻿Uninstall-ChocolateyPackage -packageName $env:ChocolateyPackageName -fileType MSI -silentArgs "{DC8185CE-C7E6-4812-9674-42919F445464} /qb /norestart" -validExitCodes @(0, 3010)
--- eset-nod32-antivirus/tools/chocolateyuninstall.ps1
+++ eset-nod32-antivirus/tools/chocolateyuninstall.ps1
@@ -1,3 +1,1 @@
-﻿# Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1" -Force
-
-Uninstall-ChocolateyPackage -packageName $env:ChocolateyPackageName -fileType MSI -silentArgs "{09517F4E-77D2-46C8-8C8D-EE66D0556D3D} /qb /norestart" -validExitCodes @(0, 3010)
+﻿Uninstall-ChocolateyPackage -packageName $env:ChocolateyPackageName -fileType MSI -silentArgs "{09517F4E-77D2-46C8-8C8D-EE66D0556D3D} /qb /norestart" -validExitCodes @(0, 3010)
