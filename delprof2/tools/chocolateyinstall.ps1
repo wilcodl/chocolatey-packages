3b539bfb33b299f2366e5bd89c4b0d10474b4a5c
Wilco de Lange <wilcodl@proton.me>
2026-06-06 22:09:44 +0200

    url fixed

--- delprof2/tools/chocolateyinstall.ps1
+++ delprof2/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
 	packageName   = $env:ChocolateyPackageName
 	unzipLocation = $toolsDir
-	url           = 'https://helgeklein.com/downloads/DelProf2/current/Delprof2 1.6.0.zip'
+	url           = 'https://helgeklein.com/files/DelProf2/current/Delprof2%201.6.0.zip'
 	checksum      = '14EE8BAF68AC5B015613ECDF23D1CC49A7045F791E2F7A1952F32AAD5B4A606B'
 	checksumType  = 'sha256'
 }
